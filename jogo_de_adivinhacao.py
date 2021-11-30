a8602e7313de49780d0cd039ae1be32c1c1d1899
Sávio Miranda <saviosousamm68@gmail.com>
2021-11-30 12:07:04 -0300

    Algumas correções na legibilidade do código das regras e inclusão de sistema de pontuação.

--- jogo_de_adivinhacao.py
+++ jogo_de_adivinhacao.py
@@ -3,15 +3,17 @@ import regras
 print(36 * '*')
 print('Bem-vindo(a) ao jogo de adivinhação!')
 print(36 * '*', end='\n\n')
+print('A pontuação máxima para cada dificuldade é:\n'
+      '20 em Fácil, 40 em Médio e 60 em Difícil.\n')
 
-DIFICULDADE_DAS_REGRAS = {'1': lambda: regras.jogo((6, 7)),
-                          '2': lambda: regras.jogo((5, 20)),
-                          '3': lambda: regras.jogo((4, 30))}
+DIFICULDADE_DAS_REGRAS = {'1': lambda: regras.jogo((6, 10, 20)),
+                          '2': lambda: regras.jogo((5, 20, 40)),
+                          '3': lambda: regras.jogo((4, 30, 60))}
 
 jogar = True
 while jogar:
     dificuldade = input('Escolha a dificuldade pressionando as seguintes teclas:\n'
-                        '[1] Fácil, [2] Médio, [3], Difícil, ou qualquer tecla para SAIR\n'
+                        '[1] Fácil, [2] Médio, [3] Difícil ou entre com qualquer tecla para SAIR\n'
                         '-> ')
 
     if dificuldade not in DIFICULDADE_DAS_REGRAS:
--- regras.py
+++ regras.py
@@ -1,20 +1,36 @@
 import random
 
 
-def jogo(regras):
-    numero_de_rodadas, limite_do_jogo = regras
+def jogo(nivel):  # O nível do jogo é baseado no número de rodadas e no limite do jogo (de 1 até n).
+    numero_de_rodadas, limite_do_jogo, pontuacao_final = nivel
+
     numero_secreto = random.randrange(1, limite_do_jogo + 1)
     print('Digite um número de 1 até {}!'.format(limite_do_jogo))
+
     for rodada in range(1, numero_de_rodadas):
         print('Rodada: {} de {}!'.format(rodada, numero_de_rodadas - 1))
         chute = int(input('-> '))
+
         if (1 > chute) or (limite_do_jogo < chute):
             print('Entrada inválida. Por favor, atenha-se às regras.')
             continue
+
         if chute == numero_secreto:
             print('Você conseguiu! O número secreto era {}!'.format(numero_secreto))
             break
-        elif chute > numero_secreto:
-            print('Errou! O número digitado é MAIOR do que o número secreto!')
+
         else:
-            print('Errou! O número digitado é MENOR do que o número secreto!')
+            if rodada == numero_de_rodadas - 1:
+                print('Você perdeu! O número secreto era:', numero_secreto)
+                pontuacao_final = 0
+                break
+
+            if chute > numero_secreto:
+                print('Errou! O número digitado é MAIOR do que o número secreto!')
+
+            else:
+                print('Errou! O número digitado é MENOR do que o número secreto!')
+
+            pontuacao_final -= abs(chute - numero_secreto)
+
+    return print("Pontuação:", pontuacao_final)
